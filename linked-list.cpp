d0e50efda6c3168f2a71e50f3d9d33d885f6bf3a
Omkar Rane <omkarrane10@gmail.com>
2019-09-27 01:37:00 +0530

    remove and print functions added

--- linked-list.cpp
+++ linked-list.cpp
@@ -29,25 +29,27 @@ class DoublyLinkedList
     Node<T>* tail = NULL;
     int size = 0;
 
-    void add_last(T elem);
+    void add_last(T);
     int length();
     bool empty();
+    void add_at(T, int);
+    Node<T>* remove_last();
+    Node<T>* remove_at(int);
+    void print();
 };
 
 type_n
 void DoublyLinkedList<T>::add_last(T elem)
 {
-  Node<T>* temp_node = new Node<T>(elem);
   if (empty())
-    head = tail = temp_node;
+    head = tail = new Node<T>(elem);
   else
   {
-    tail->next = temp_node;
+    tail->next = new Node<T>(elem);
     tail = tail->next;
   }
 
   size++;
-  free(temp_node);
 };
 
 type_n
@@ -62,11 +64,75 @@ bool DoublyLinkedList<T>::empty()
   return length() == 0;
 };
 
+type_n
+void DoublyLinkedList<T>::add_at(T elem,int pos)
+{
+  int curr_pos = 1;
+  Node<T>* itr = head;
+  
+  while(curr_pos < pos)
+  {
+    itr = itr->next;
+    curr_pos++;
+  };
+
+  Node<T>* new_node = new Node<T>(elem);
+  new_node->next = itr->next;
+  new_node->prev = itr;
+  itr->next->prev = new_node;
+  itr->next = new_node;
+  size++;
+};
+
+type_n
+Node<T>* DoublyLinkedList<T>::remove_last()
+{
+  Node<T>* itr = tail;
+  tail->prev->next = NULL;
+  tail = tail->prev;
+  free(itr);
+  size--;
+};
+
+type_n
+Node<T>* DoublyLinkedList<T>::remove_at(int pos)
+{
+  int curr_pos = 0;
+  Node<T>* itr = head;
+  
+  while(curr_pos <= pos)
+  {
+    itr = itr->next;
+    curr_pos++;
+  };
+
+  itr->prev->next = itr->next;
+  itr->next->prev = itr->prev;
+  free(itr);
+  size--;
+};
+
+type_n
+void DoublyLinkedList<T>::print()
+{
+  Node<T>* itr = head;
+  while(itr != NULL)
+  {
+    cout << itr->data << endl;
+    itr = itr->next;
+  };
+};
+
 int main()
 {
   DoublyLinkedList<string> dll = DoublyLinkedList<string>();
   dll.add_last("abc");
   dll.add_last("def");
-  cout << "Length -> " << dll.length();
+  dll.add_last("xyz");
+  dll.add_at("pqr", 2);
+  dll.remove_at(1);
+  dll.remove_last();
+  dll.print();
+  
   return 0;
 }
