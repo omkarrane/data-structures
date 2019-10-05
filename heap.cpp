0316ec6025531b8465c9d82b8bc22910967f2881
Omkar Rane <omkarrane10@gmail.com>
2019-10-05 23:21:58 +0530

    renamed priority-queue to heap and implemented heapify operations

--- heap.cpp
+++ heap.cpp
@@ -11,7 +11,7 @@ private:
   int size = 0;
 
 public:
-  Heap(int);
+  Heap();
 
   int get_left_child_index(int);
   int get_right_child_index(int);
@@ -31,17 +31,17 @@ public:
   T peek();
   T poll();
   void insert(T);
+  void swap_elements(int, int);
 };
 
 type_n
-Heap<T>::Heap(int size)
+Heap<T>::Heap()
 {
-  this->size = size;
 };
 
 type_n int Heap<T>::get_left_child_index(int index)
 {
-  if (index == 0)
+  if (index < 0)
     throw "Invalid Index Exception";
 
   return index * 2 + 1;
@@ -49,7 +49,7 @@ type_n int Heap<T>::get_left_child_index(int index)
 
 type_n int Heap<T>::get_right_child_index(int index)
 {
-  if (index == 0)
+  if (index < 0)
     throw "Invalid Index Exception";
 
   return index * 2 + 2;
@@ -101,7 +101,7 @@ type_n T Heap<T>::get_parent(int index)
 
 type_n bool Heap<T>::has_parent(int index)
 {
-  return get_parent_index(index) > 0;
+  return get_parent_index(index) >= 0;
 };
 
 type_n T Heap<T>::peek()
@@ -119,6 +119,7 @@ type_n T Heap<T>::poll()
 
   T removed_element = arr[0];
   arr[0] = arr[arr.size() - 1];
+  arr.pop_back();
   size--;
   heapify_down();
   return removed_element;
@@ -126,27 +127,82 @@ type_n T Heap<T>::poll()
 
 type_n void Heap<T>::insert(T element)
 {
-  if(arr.size() > size)
-    throw "Heap Full Exception";
-
-  arr.insert(element);
+  arr.push_back(element);
   size++;
   heapify_up();
 };
 
 type_n void Heap<T>::heapify_up()
 {
-
+  int index = size - 1;
+  while(has_parent(index) && get_element_at(index) > get_parent(index))
+  {
+    swap_elements(index, get_parent_index(index));
+    index = get_parent_index(index);
+  };
 };
 
 type_n void Heap<T>::heapify_down()
 {
+  int index = 0;
+  while(has_left_child(index))
+  {
+    int smallest_child_index = get_left_child_index(index);
 
+    if(has_right_child(index) && get_element_at(smallest_child_index) > get_right_child(index) && get_element_at(index) > get_right_child(index))
+      smallest_child_index = get_right_child_index(index);
+
+    if(get_element_at(smallest_child_index) > get_element_at(index))
+    {
+      swap_elements(index, smallest_child_index);
+      index = smallest_child_index;
+    }
+    else
+      break;
+  }
+};
+
+type_n void Heap<T>::swap_elements(int pos1, int pos2)
+{
+  T temp_elem = arr[pos1];
+  arr[pos1] = arr[pos2];
+  arr[pos2] = temp_elem;
 };
 
 
 int main()
 {
-  Heap<int> h = Heap<int>(10);
+  Heap<int> h = Heap<int>();
+  h.insert(15);
+  h.insert(10);
+  h.insert(20);
+  h.insert(17);
+  h.insert(25);
+  cout << h.poll() << endl;
+  h.insert(50);
+  cout << h.poll() << endl;
+  cout << h.poll() << endl;
+  cout << h.poll() << endl;
+
   return 0;
 }
+
+
+
+/*
+      15
+      
+      15
+    10
+
+      15        20
+    10  20    10  15
+
+      20        20
+    10  15    17  15
+  17        10  
+
+      20        20         25
+    17  15    25  15     20   15
+  10  25    10  17      10  17
+*/
